8f56e4111bc8ac803f1629c93691131f5405d217
Rajkiran Panuganti <rajkiran.panuganti@gmail.com>
2017-03-25 16:50:03 -0700

    Initial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import {AngularFireModule} from 'angularfire2';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
+export const firebaseConfig = {
+
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +16,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
